1afac1ad6346292d005e075d0275768d7aa35511
queq1890 <mtmtatmat4@toki.waseda.jp>
2018-04-17 17:47:03 +0900

    フォームを作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,18 @@
 class TasksController < ApplicationController
 
   def index
+    @task = Task.new
+  end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :start_at, :finish_at)
   end
 end
